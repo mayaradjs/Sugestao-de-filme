c77a67421d115646ed07983d8ccdeafb9aea7e3e
Mayara Silva <maydkt@gmail.com>
2018-11-03 18:19:54 -0300

    Inclusão do modal e iniciado a chamada a movies.html

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,5 @@
 const keyEnter = 13;
 
-
 $(document).ready(function() {
   $('#divPrincipal input,#divPrincipal select').keyup(function(event) {
     if (event.keyCode == keyEnter)
@@ -9,5 +8,55 @@ $(document).ready(function() {
 });
 
 function buscar() {
-  alert("//TODO buscar filme")
+  $("#modal-loading").show();
+  trocarContexto("filme");
+  $.ajax({
+      url: 'api/movies.html',
+      type: 'GET',
+      dataType: 'html'
+    })
+    .done(function(result) {
+      console.log(result);
+      var $html = $(result);
+      //TODO Selecionar o filme baseado nos filtros
+    })
+    .fail(function(result) {
+      trocarContexto("principal");
+      alert("Algo não ocorreu como deveria.");
+    })
+    .always(function() {
+      $("#modal-loading").hide();
+      console.debug("Request Complete");
+    });
+
+}
+
+function trocarContexto(contexto) {
+  switch (contexto) {
+    case "principal":
+      limpaFiltros();
+      divHide($("#divFilme"));
+      divShow($("#divPrincipal"));
+      break;
+    case "filme":
+      divHide($("#divPrincipal"));
+      divShow($("#divFilme"));
+      break;
+  }
+}
+
+function divShow($element) {
+  $element.show(400, function() {});
+  //$element.removeClass('div-hide');
+  //$element.addClass('div-show');
+}
+
+function divHide($element) {
+  $element.hide(400, function() {});
+  //$element.removeClass('div-show');
+  //$element.addClass('div-hide');
+}
+
+function limpaFiltros() {
+  $('#divPrincipal input,#divPrincipal select').val("");
 }
